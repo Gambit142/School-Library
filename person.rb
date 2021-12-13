a942e9c045b919b27300a03d66a8a2356de6b824
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-13 23:21:54 +0100

    Make changes according to suggestion of the code reviewer

--- person.rb
+++ person.rb
@@ -1,12 +1,12 @@
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = rand 1..1000
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  attr_reader :id
-  attr_accessor :name, :age
 
   def can_use_services
     @age >= 18 || @parent_permission == true
@@ -15,5 +15,5 @@ class Person
   def of_age?
     @age >= 18
   end
-  private :is_of_age
+  private :of_age
 end
