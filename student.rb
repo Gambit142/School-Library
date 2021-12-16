358d9475cc4dd6d448be7fe653a775356bc0bdf9
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-16 11:42:32 +0100

    Include a rental instance variable in the student class

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
   def initialize(age, name, classroom, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
