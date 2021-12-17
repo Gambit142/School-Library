b419788ce636e48ee05c08b565741816ea8f2514
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-17 14:17:44 +0100

    Create a class that holds all options that will be shown and print them when the file is executed

--- main.rb
+++ main.rb
@@ -123,3 +123,55 @@ class OptionMethods
   end
 end
 
+class ListOptions
+  def self.question_page
+    puts 'Welcome to the OOP School Library App!'
+    puts "\n"
+    puts 'Please choose an option by entering a number: '
+    option1 = '1 - List all books'
+    option2 = '2 - List all people'
+    option3 = '3 - Create a person'
+    option4 = '4 - Create a book'
+    option5 = '5 - Create a rental'
+    option6 = '6 - List all rentals for a given person id'
+    option7 = '7 - Exit'
+
+    questions_array = [
+      option1, option2, option3, option4, option5, option6, option7
+    ]
+
+    questions_array.each { |question| puts question }
+    Integer(gets.chomp)
+  end
+
+  option_method = OptionMethods.new
+
+  loop do
+    case question_page
+    when 1
+      option_method.book_list
+    when 2
+      option_method.people_list
+    when 3
+      option_method.create_person
+    when 4
+      option_method.create_book
+    when 5
+      option_method.create_rental
+    when 6
+      option_method.rental_list
+    when 7
+      puts 'Thank you for using the app!'
+      exit
+    else
+      puts 'Choose a number between 1 to 7'
+      exit
+    end
+  end
+end
+
+def main
+  ListOptions.new
+end
+
+main
