6b9538f1a9b2d8995b13c03a62b2520bf89f656c
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-23 15:39:42 +0100

    Write tests to check the behaviour of the Person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,17 @@
+require './person.rb'
+
+describe Person do 
+  before(:each) do 
+     @person = Person.new(325, 40, 'Hector')
+  end 
+  
+  it 'should be able to use services' do
+   permission = @person.can_use_services
+   expect(permission).to be true 
+  end 
+  
+  it 'should have its own attributes' do
+   expect(@person).to have_attributes(age: 40) 
+   expect(@person).to have_attributes(name: 'Hector')
+  end
+end
