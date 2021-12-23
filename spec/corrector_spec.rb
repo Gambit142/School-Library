31c1cc4452457ec21e90b4351ddac60f6264a4a7
Whoistolu <digittolu25@gmail.com>
2021-12-23 15:50:49 +0100

    Add unit tests to the Corrector class

---
+++ spec/corrector_spec.rb
@@ -0,0 +1,16 @@
+require './corrector.rb'
+
+describe Corrector do 
+  before(:each) do 
+    @corrector = Corrector.new
+    @corrected_name = @corrector.correct_name('alexandriasss')
+  end 
+  
+  it 'should be an instance of Classroom' do
+    expect(@corrector).to be_instance_of Corrector
+  end 
+  
+  it 'should correct a name that is loneger than ten characters' do
+   expect(@corrected_name).to eql 'Alexandria'
+  end
+end
