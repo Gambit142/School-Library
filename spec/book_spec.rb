6b3157f4861e4b4ebc79278bcb84384d55d3105b
Whoistolu <digittolu25@gmail.com>
2021-12-23 15:46:51 +0100

    Add unit tests to the Book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,17 @@
+require './book.rb'
+
+describe Book do 
+  before(:each) do 
+     @book = Book.new('Merchant of Venice', 'Williams Shakespeare')
+  end 
+  
+  it 'should print a string with the title and author' do
+   output = @book.to_s
+   expect(output).to eql "Title: Merchant of Venice, Author: Williams Shakespeare"
+  end 
+  
+  it 'should have its own attributes' do
+   expect(@book).to have_attributes(title: 'Merchant of Venice') 
+   expect(@book).to have_attributes(author: 'Williams Shakespeare')
+  end
+end
